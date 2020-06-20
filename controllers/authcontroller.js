871c34be3679c480b9a3568b12f640d8fb480e18
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-20 16:32:31 +0100

    refactored signup to correct file

---
+++ controllers/authcontroller.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const _ = require('lodash')
+const jwt = require('jsonwebtoken')
+const User = require('../models/usermodel')
+
+
+exports.signup = async (req,res) => {
+  const email = _.toLower(req.body.email);
+  const username = _.toLower(req.body.username);
+
+  User emailUser = await User.findOne({email})
+
+}
