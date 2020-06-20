c0a7e6ca91bbd5a5ccfcf63ae9251bc1de226c00
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-20 16:27:34 +0100

    add auth route, other middleware

---
+++ app.js
@@ -0,0 +1,43 @@
+require ('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan')
+const cLog = require('./utils/Custom-Logging')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const expressValidator = require('express-validator')
+
+
+const app = new express();
+
+//routes
+const authRoute = require('./routes/authroute')
+
+
+//middleware
+app.use(morgan(':method :remote-addr :url :status :response-time ms :res[content-length]'));
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(cors({
+  origin : true,
+  credentials : true
+}))
+app.options('*', cors())
+
+app.use("/auth", authRoute)
+
+app.get("/", (req,res) => res.send("Home page"))
+
+
+
+console.log("\n")
+mongoose.connect(process.env.DB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser : true
+}).then(() => cLog.info('Database connected '))
+mongoose.connection.on('error', err => cLog.error("Error connecting to database: " + err ))
+
+
+const port = process.env.port || 5000;
+app.listen(port, () => cLog.info(`Server started on port ${port}`))
