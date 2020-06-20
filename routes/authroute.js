645f7d3341af22166c4c3e7b9a70bacb8bcd3411
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-20 16:33:04 +0100

    refactored signup to correct file

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const {signup} = require('../controllers/usercontroller')
+const {signup} = require('../controllers/authcontroller')
 
 router.get("/signup", signup)
 
