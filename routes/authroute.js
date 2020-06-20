954467184b52b31d0978663af531ecb40da545f2
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-20 21:00:48 +0100

    signin  route

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const {signup} = require('../controllers/authcontroller')
+const {signup,signin} = require('../controllers/authcontroller')
+
 
 router.post("/signup", signup)
+router.post("/signin", signin)
 
 module.exports = router
