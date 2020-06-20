5cc982d69213601639918a31822b9aaa4db79dab
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-20 16:36:38 +0100

    change signup to POST

--- routes/authroute.js
+++ routes/authroute.js
@@ -3,6 +3,6 @@ const router = express.Router()
 
 const {signup} = require('../controllers/authcontroller')
 
-router.get("/signup", signup)
+router.post("/signup", signup)
 
 module.exports = router
