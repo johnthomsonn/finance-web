bd4e183ed81a848b9d5606dfe859676a8f73b933
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 13:19:29 +0100

    validation for sign  up/in

---
+++ validation/authvalidation.js
@@ -0,0 +1,55 @@
+const {check, validationResult} = require("express-validator/check");
+
+exports.getSignupErrors = [
+  check("username", "username is required")
+    .not()
+    .isEmpty()
+    .custom(value => !/@/.test(value))
+    .withMessage("Username cannot contain the @ symbol")
+    .custom(value => !/[<\s:[\]/>\\'"!]/.test(value))
+    .withMessage("username cannot contain spaces or special characters")
+    .trim(),
+
+  check("email", "A valid email is required")
+    .not()
+    .isEmpty()
+    .isEmail()
+    .normalizeEmail(),
+
+  check("password", "A password is required")
+    .not()
+    .isEmpty()
+    .isLength({min: 8})
+    .withMessage("Password must be at leat 8 characters")
+];
+
+exports.signupValidation = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errArray = errors.array();
+    const msgs = errArray.map(error => error.msg);
+    return res.status(400).json({error: msgs});
+  }
+  next();
+};
+
+exports.getSignInErrors = ([
+  check("unique", "Must be filled in")
+    .not()
+    .isEmpty(),
+  check("password", "A password is required")
+    .not()
+    .isEmpty()
+    .isLength({min: 8})
+    .withMessage("Password must be at leat 8 characters")
+]);
+
+exports.signinValidation = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errArray = errors.array()
+    const msgs = errArray.map(error => error.msg)
+    return res.status(400).json({error : msgs})
+  }
+  next();
+}
