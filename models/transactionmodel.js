b5263839ea961f922566ebb34bd892f241768c90
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 15:53:26 +0100

    transaction monel

--- models/transactionmodel.js
+++ models/transactionmodel.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoos')
 const {ObjectId} = mongoose.Schema
 
+const transacitonCategories = ['bills', 'salary', 'food', 'other', 'bad']
+
 const transactionSchema = new mongoose.Schema({
   description : {
     type : String,
@@ -19,7 +21,16 @@ const transactionSchema = new mongoose.Schema({
     type : String,
     enum : ['income' , 'expenditure'],
     required : true
+  },
+  category : {
+    type : String,
+    enum : transacitonCategories,
+    required : true
   }
 })
+
+transactionSchema.methods = {
+  getCategories : () => transacitonCategories
+}
 
  module.exports = mongoose.model("Transaction", transactionSchema)
