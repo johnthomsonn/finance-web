a85781637bbeccd5ff3bf9726fd27c506d3186a4
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 20:00:04 +0100

    delete transaction

--- controllers/transactioncontroller.js
+++ controllers/transactioncontroller.js
@@ -2,6 +2,62 @@ const express = require('express')
 const cLog = require('../utils/Custom-Logging')
 const Transaction = require('../models/transactionmodel')
 
+// this will check the transaction user id matches the logged in user id
+exports.getTransactionById = async (req,res,next, id) => {
+
+  try
+  {
+    const foundTransaction = await Transaction.findById(id)
+    if(foundTransaction && foundTransaction != undefined)
+    {
+      req.transaction = foundTransaction
+      next()
+    }
+    else
+    {
+      return res.status(400).json({
+        error : "Error finding transaction"
+      })
+    }
+  }
+  catch (err)
+  {
+    cLog.error("Error finding transaction by ID");
+    return res.status(400).json({
+      error : "Error finding transaction"
+    })
+  }
+}
+
+exports.deleteTransaction = (req,res) => {
+  Transaction.findOneAndDelete({_id : req.transaction._id}, (err, removedTransaction) => {
+    if(err || !removedTransaction)
+    {
+      return res.status(400).json({
+        error : "Could not delete transaction"
+      })
+    }
+    else
+    {
+      //transaction has been deleted so remove from user transactions
+      const newArray = req.user.transactions.filter(transaction => transaction._id.toString() != removedTransaction._id.toString())
+      req.user.transactions = newArray
+      req.user.save((err,doc) => {
+        if(err || !doc){
+          return res.json({
+            error : "Error deleting the transaction"
+          })
+        }
+        else
+        {
+          return res.json({
+            message : `transaction ${removedTransaction.description} deleted`
+          })
+        }
+      })
+    }
+  })
+}
 
 exports.addTransaction = async (req,res) => {
   try
@@ -21,7 +77,8 @@ exports.addTransaction = async (req,res) => {
     const savedUser = user.save()
 
     return res.json({
-      message : "transaction " + savedTransaction.description + " saved."
+      message : "transaction " + savedTransaction.description + " saved.",
+      transaction_id : savedTransaction._id
     })
 
   }
