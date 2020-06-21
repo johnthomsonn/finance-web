c6f3124298e65f6e9625c00fa8d22c030e34db45
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 19:07:31 +0100

    adding transaction is validated

--- routes/transactionroute.js
+++ routes/transactionroute.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router();
 
 const {addTransaction} = require('../controllers/transactioncontroller')
+const {getTransactionCreationErrors,transactionValidation} = require('../validation/transactionvalidation')
 
-router.post("/", addTransaction)
+router.post("/", getTransactionCreationErrors, transactionValidation,  addTransaction)
 
 module.exports = router;
