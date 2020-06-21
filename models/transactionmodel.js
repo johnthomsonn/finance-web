326e729b0c0900f5b1ed4ff8d442558f0d0efca4
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 17:57:35 +0100

    refactor schema code to separate file

--- models/transactionmodel.js
+++ models/transactionmodel.js
@@ -1,36 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema
+const transactionSchemara = require('./transactionmodelschema')
 
-const transacitonCategories = ['bills', 'salary', 'food', 'other', 'bad']
 
-const transactionSchema = new mongoose.Schema({
-  description : {
-    type : String,
-    required : true
-  },
-  amount : {
-    type : Number,
-    required: true
-  },
-  user : {
-    type : ObjectId,
-    ref : "User"
-  },
-  transactionType: {
-    type : String,
-    enum : ['income' , 'expenditure'],
-    required : true
-  },
-  category : {
-    type : String,
-    enum : transacitonCategories,
-    required : true
-  }
-})
 
-transactionSchema.methods = {
-  getCategories : () => transacitonCategories
-}
-
- module.exports = mongoose.model("Transaction", transactionSchema)
+module.exports = mongoose.model("Transaction", transactionSchema)
