9a6fc86113a602b5bdb1a38ae1b251fbe1c60b86
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 20:01:08 +0100

    delete transaction route

--- routes/transactionroute.js
+++ routes/transactionroute.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const {addTransaction} = require('../controllers/transactioncontroller')
+const {addTransaction,getTransactionById,deleteTransaction } = require('../controllers/transactioncontroller')
 const {getTransactionCreationErrors,transactionValidation} = require('../validation/transactionvalidation')
 
 router.post("/", getTransactionCreationErrors, transactionValidation,  addTransaction)
+router.delete("/:transactionId",  deleteTransaction)
+
+router.param("transactionId", getTransactionById )
 
 module.exports = router;
