80eb362a436eb74492f09a6c94fb90d915ac6dbc
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 13:19:15 +0100

    signup/sign in now validates

--- routes/authroute.js
+++ routes/authroute.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const {signup,signin, signout} = require('../controllers/authcontroller')
+const {getSignupErrors, signupValidation, getSignInErrors, signinValidation} = require('../validation/authvalidation')
 
 
-router.post("/signup", signup)
-router.post("/signin", signin)
+router.post("/signup", getSignupErrors, signupValidation, signup)
+router.post("/signin", getSignInErrors, signinValidation, signin)
 router.get("/signout", signout)
 
 module.exports = router
