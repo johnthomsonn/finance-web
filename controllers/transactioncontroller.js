7659bd72483c6763a34b64c7f3b7b36373377101
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 20:05:06 +0100

    refactor

--- controllers/transactioncontroller.js
+++ controllers/transactioncontroller.js
@@ -1,93 +1,88 @@
-const express = require('express')
-const cLog = require('../utils/Custom-Logging')
-const Transaction = require('../models/transactionmodel')
+const express = require("express");
+const cLog = require("../utils/Custom-Logging");
+const Transaction = require("../models/transactionmodel");
 
 // this will check the transaction user id matches the logged in user id
-exports.getTransactionById = async (req,res,next, id) => {
-
-  try
-  {
-    const foundTransaction = await Transaction.findById(id)
-    if(foundTransaction && foundTransaction != undefined)
-    {
-      req.transaction = foundTransaction
-      next()
-    }
-    else
-    {
+exports.getTransactionById = async (req, res, next, id) => {
+  try {
+    const foundTransaction = await Transaction.findById(id);
+    if (foundTransaction && foundTransaction != undefined) {
+      req.transaction = foundTransaction;
+      next();
+    } else {
       return res.status(400).json({
-        error : "Error finding transaction"
-      })
+        error: "Error finding transaction"
+      });
     }
-  }
-  catch (err)
-  {
+  } catch (err) {
     cLog.error("Error finding transaction by ID");
     return res.status(400).json({
-      error : "Error finding transaction"
-    })
+      error: "Error finding transaction"
+    });
   }
-}
+};
 
-exports.deleteTransaction = (req,res) => {
-  Transaction.findOneAndDelete({_id : req.transaction._id}, (err, removedTransaction) => {
-    if(err || !removedTransaction)
-    {
-      return res.status(400).json({
-        error : "Could not delete transaction"
-      })
+exports.deleteTransaction = (req, res) => {
+  Transaction.findOneAndDelete(
+    {_id: req.transaction._id},
+    (err, removedTransaction) => {
+      if (err || !removedTransaction) {
+        return res.status(400).json({
+          error: "Could not delete transaction"
+        });
+      } else {
+        //transaction has been deleted so remove from user transactions
+        const newArray = req.user.transactions.filter(
+          transaction =>
+            transaction._id.toString() != removedTransaction._id.toString()
+        );
+        req.user.transactions = newArray;
+        req.user.save((err, doc) => {
+          if (err || !doc) {
+            return res.json({
+              error: "Error deleting the transaction"
+            });
+          } else {
+            return res.json({
+              message: `transaction ${removedTransaction.description} deleted`
+            });
+          }
+        });
+      }
     }
-    else
-    {
-      //transaction has been deleted so remove from user transactions
-      const newArray = req.user.transactions.filter(transaction => transaction._id.toString() != removedTransaction._id.toString())
-      req.user.transactions = newArray
-      req.user.save((err,doc) => {
-        if(err || !doc){
-          return res.json({
-            error : "Error deleting the transaction"
-          })
-        }
-        else
-        {
-          return res.json({
-            message : `transaction ${removedTransaction.description} deleted`
-          })
-        }
-      })
-    }
-  })
-}
+  );
+};
 
-exports.addTransaction = async (req,res) => {
-  try
-  {
-    const {description, amount, transactionType, category} = req.body
+exports.addTransaction = async (req, res) => {
+  try {
+    const {description, amount, transactionType, category} = req.body;
 
     const newTransaction = await new Transaction({
       description,
       amount,
       transactionType,
       category,
-      user : req.user
-    })
+      user: req.user
+    });
     const savedTransaction = await newTransaction.save();
-    const user = req.user
-    user.transactions.push(savedTransaction)
-    const savedUser = await user.save()
-
-    return res.json({
-      message : "transaction " + savedTransaction.description + " saved.",
-      transaction_id : savedTransaction._id
-    })
-
-  }
-  catch(err)
-  {
-    cLog.error("Error adding transaction: " + err)
+    if (savedTransaction != undefined) {
+      const user = req.user;
+      user.transactions.push(savedTransaction);
+      const savedUser = await user.save();
+      if (savedUser != undefined) {
+        return res.json({
+          message: "transaction " + savedTransaction.description + " saved.",
+          transaction_id: savedTransaction._id
+        });
+      }
+    }
     return res.status(400).json({
-      error : "Error when trying to add the transaction."
-    })
+      error: "Error when trying to add the transaction."
+    });
+  } catch (err) {
+    cLog.error("Error adding transaction: " + err);
+    return res.status(400).json({
+      error: "Error when trying to add the transaction."
+    });
   }
-
-}
+};
