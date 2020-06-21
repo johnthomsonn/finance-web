7032a6657b2b6843c95b3a374756407522ec2847
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-21 17:49:43 +0100

    get username from username in url

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const User = require('../models/usermodel')
+const _ = require('lodash')
+const cLog = require('../utils/Custom-Logging')
 
-exports.getuserByParam = (req,res, next) => {
-  
+exports.getUserByUsernameParam = (req,res, next, username) => {
+User.findOne({username}, (err,user) => {
+  if(err || !user)
+  {
+    cLog.error("Error finding user by username param: " + err)
+    return res.status(400).json({
+      error : "Could not get user from username in URL"
+    })
+  }
+  else
+  {
+    req.user = user;
+    next()
+  }
+})
+
 }
