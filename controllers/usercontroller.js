490f14264e9339762f5fdb2553a5fb44a7606021
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-22 12:39:49 +0100

    refactored

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,22 +1,18 @@
-const express = require('express')
-const User = require('../models/usermodel')
-const _ = require('lodash')
-const cLog = require('../utils/Custom-Logging')
+const express = require("express");
+const User = require("../models/usermodel");
+const _ = require("lodash");
+const cLog = require("../utils/Custom-Logging");
 
-exports.getUserByUsernameParam = (req,res, next, username) => {
-User.findOne({username}, (err,user) => {
-  if(err || !user)
-  {
-    cLog.error("Error finding user by username param: " + err)
-    return res.status(400).json({
-      error : "Could not get user from username in URL"
-    })
-  }
-  else
-  {
-    req.user = user;
-    next()
-  }
-})
-
-}
+exports.getUserByUsernameParam = (req, res, next, username) => {
+  User.findOne({username}, (err, user) => {
+    if (err || !user) {
+      cLog.error("Error finding user by username param: " + err);
+      return res.status(400).json({
+        error: "Could not get user from username in URL"
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
