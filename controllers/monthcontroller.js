72aaf0d1f851c4ea5789555d1ef7a2e63883d68c
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-22 10:00:50 +0100

    adds month to req from url

--- controllers/monthcontroller.js
+++ controllers/monthcontroller.js
@@ -1,7 +1,61 @@
 const express = require('express')
 const cLog = require('../utils/Custom-Logging')
+const _ = require('lodash')
 
 exports.getMonthByUrlParam = (req,res, next, month) => {
-  const [monthUrl, day] = month.split("-")
-  cLog.debug(monthUrl + ":" + day)
+  const [monthUrl, year] = month.split("-")
+  const monthNum = getMonthNumberFromString(_.toLower(monthUrl))
+  if(monthNum === 0)
+  {
+    return res.status(400).json({
+      error : "Invalid month, please enusre it is in the form of mmm-yy."
+    })
+  } 
+  req.month = monthNum + "-" + year
+  next()
+
+}
+
+const getMonthNumberFromString = (monthStr) => {
+  let num = 0;
+  switch(monthStr)
+  {
+    case  "jan" :
+      num = 1;
+      break
+      case "feb" :
+      num = 2
+      break
+      case "mar" :
+      num = 3
+      break
+      case "apr" :
+      num = 4
+      break
+      case  "may" :
+        num = 5;
+        break
+        case "jun" :
+        num = 6
+        break
+        case "jul" :
+        num = 7
+        break
+        case "aug" :
+        num = 8
+        break
+        case  "sep" :
+          num = 9;
+          break
+          case "oct" :
+          num = 10
+          break
+          case "nov" :
+          num = 11
+          break
+          case "dec" :
+          num = 12
+          break
+  }
+  return num;
 }
