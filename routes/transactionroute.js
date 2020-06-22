8d533ae6d78016c9157818670e706c49a5b40af1
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-22 08:32:58 +0100

    get all transactions for month

--- routes/transactionroute.js
+++ routes/transactionroute.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-const {addTransaction,getTransactionById,deleteTransaction } = require('../controllers/transactioncontroller')
+const {addTransaction,getTransactionById,deleteTransaction, getAllTransactionsForMonth } = require('../controllers/transactioncontroller')
 const {getTransactionCreationErrors,transactionValidation} = require('../validation/transactionvalidation')
 
 router.post("/", getTransactionCreationErrors, transactionValidation,  addTransaction)
 router.delete("/:transactionId",  deleteTransaction)
+router.get("/", getAllTransactionsForMonth)
 
 router.param("transactionId", getTransactionById )
 
