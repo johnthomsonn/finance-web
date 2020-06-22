204671c6204e8fbfd2b6b6a95770073cfc60af88
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-22 20:14:06 +0100

    Home style sheet first

--- react-frontend/src/components/core/Home.js
+++ react-frontend/src/components/core/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import './Home.css'
 const Home = props => {
 
 
   return (<>
+    <div className="header">
     Home page
+    </div>
   </>)
 }
 export default Home
