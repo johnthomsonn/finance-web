aba982516b4463131bc969294509901312ea8f4e
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-22 17:15:16 +0100

    delete user route

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const {signup,signin, signout} = require('../controllers/authcontroller')
+const {signup,signin, signout, deleteUser} = require('../controllers/authcontroller')
 const {getSignupErrors, signupValidation, getSignInErrors, signinValidation} = require('../validation/authvalidation')
 
 
 router.post("/signup", getSignupErrors, signupValidation, signup)
 router.post("/signin", getSignInErrors, signinValidation, signin)
 router.get("/signout", signout)
+router.delete("/", deleteUser )
 
 module.exports = router
