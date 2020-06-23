e317adf9a2f100d697d6ccc25ff4e14aed4b4ff9
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-23 11:49:13 +0100

    took out navbar

--- react-frontend/src/components/core/Home.js
+++ react-frontend/src/components/core/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 import './Home.css'
+
+//todo:
+// render links based on signed in or not
+// change signout link to styled button
 
 const Home = props => {
 
   return (<>
+
+
+
     <div className="header">
     Home page
     </div>
