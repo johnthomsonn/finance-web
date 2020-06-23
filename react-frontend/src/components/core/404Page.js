8addfd673d4e4fa21e80045137399dee9c8a6329
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-23 11:48:53 +0100

    Page not found page

---
+++ react-frontend/src/components/core/404Page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page404 = props => {
+
+  return (<>
+    <div className=" jumbotron alert-danger" style={{display : "flex", justifyContent:"center", fontSize : "1.75rem", border : "2px solid #a8423d"}}>
+      {` ${props.location.pathname} Page cannot be found`}
+    </div>
+  </>)
+}
+
+export default Page404
