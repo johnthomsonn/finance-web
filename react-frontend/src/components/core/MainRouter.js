48a2945713b90fb3efd22935f2c36a80fcba407d
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-23 11:49:33 +0100

    catches 404 page requests and persists navabr

--- react-frontend/src/components/core/MainRouter.js
+++ react-frontend/src/components/core/MainRouter.js
@@ -3,17 +3,23 @@ import {Switch, BrowserRouter, Route} from "react-router-dom";
 import Home from "./Home";
 import Signup from "./Signup";
 import Signin from "./Signin";
+import NavBar from '../navbar/Navbar'
+import Page404 from './404Page'
 
 const MainRouter = props => {
-  return (
+  return (<>
+
     <BrowserRouter>
+    <NavBar />
       <Switch>
         <Route path="/" exact component={Home} {...props} />
         <Route path="/signin" exact component={Signin} {...props} />
         <Route path="/signup" exact component={Signup} {...props} />
+        <Page404 {...props}/>
       </Switch>
+
     </BrowserRouter>
-  );
+  </>);
 };
 
 export default MainRouter;
