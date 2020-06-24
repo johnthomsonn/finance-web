d1e132fd3b2b8073c8b483f44271cb99898ced3c
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-24 19:51:52 +0100

    finsih signup form 2

--- react-frontend/src/components/core/Signup.js
+++ react-frontend/src/components/core/Signup.js
@@ -26,27 +26,27 @@ const Signup = props => {
       <form onSubmit={submitSignup} >
         <div className="form-group">
           <label for="username" class="bmd-label-floating"> Username </label>
-          <input type="text" class="form-control" id="username" />
+          <input type="text" class="form-control" id="username" value={input.username}/>
           <span className="bmd-help"> Username must be unique </span>
         </div>
 
           <div className="form-group">
             <label for="email" class="bmd-label-floating"> Email </label>
-            <input type="email" class="form-control" id="email" />
+            <input type="email" class="form-control" id="email" value={input.email} />
             <span className="bmd-help" > Email must be unique </span>
           </div>
 
           <div className="form-group">
             <label for="password" class="bmd-label-floating"> Password </label>
-            <input type="password" class="form-control" id="password" />
+            <input type="password" class="form-control" id="password" value={input.password} />
           </div>
 
           <div className="form-group">
             <label for="confirm" class="bmd-label-floating"> Confirm Password </label>
-            <input type="password" class="form-control" id="confirm" />
+            <input type="password" class="form-control" id="confirm" value={input.confirm}/>
           </div>
 
-          <button type="submit" className="btn btn-raised submit-button"> Sign Up </button>
+          <button type="submit" className="btn btn-raised submit-button" style={{display : (input.valid ? "" : "none")}} > Sign Up </button>
 
       </form>
     </div>
