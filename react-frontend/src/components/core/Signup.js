8768e0c92348b6fb89905f0bbf0163ce3a713b3c
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-24 16:23:37 +0100

    more signup form complete

--- react-frontend/src/components/core/Signup.js
+++ react-frontend/src/components/core/Signup.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import './Signup.css'
 
 const Signup = props => {
 
+  const submitSignup = evt => {
+    evt.preventDefault();
+
+
+
+  }
+
 
   return (<>
-    Signup page
+    <div className="signupBackground">
+    <div className="signup">
+    <h1> Sign Up </h1>
+      <form onSubmit={submitSignup} >
+        <div className="form-group">
+          <label for="username" class="bmd-label-floating"> Username </label>
+          <input type="text" class="form-control" id="username" />
+          <span className="bmd-help"> Username must be unique </span>
+        </div>
+
+          <div className="form-group">
+            <label for="email" class="bmd-label-floating"> Email </label>
+            <input type="email" class="form-control" id="email" />
+            <span className="bmd-help" > Email must be unique </span>
+          </div>
+
+      </form>
+    </div>
+    </div>
   </>)
 }
 export default Signup
