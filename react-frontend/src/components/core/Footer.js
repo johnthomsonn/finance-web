f32ba2bdb8d89b18dc15dcceb46dc016e5bc51f8
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-24 11:09:02 +0100

    footer component

---
+++ react-frontend/src/components/core/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = props => {
+  return (<>
+    <footer>
+      <p>
+        © John Thomson
+      </p>
+    </footer>
+  </>)
+}
+
+export default Footer
