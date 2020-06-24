0383c093434d5c7e877051d60a9913076bc202a0
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-06-24 10:29:48 +0100

    header section

--- react-frontend/src/components/core/Home.js
+++ react-frontend/src/components/core/Home.js
@@ -13,9 +13,17 @@ const Home = props => {
 
 
 
-    <div className="header">
-    Home page
-    </div>
+    <header>
+    <h1>Finance Tracker</h1>
+    <h6> Keeping track of your money</h6>
+    </header>
+
+    <section id="about">
+      <p>
+        Finance tracker allows you to create your own private profile where you can then add each financial transaction
+        and categorise them which allows you to see exactly where your money is going each month.
+      </p>
+    </section>
   </>)
 }
 export default Home
