7c8bab602e3f4c22dec5083ee7e7140ee4fe28b4
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-06 11:26:03 +0100

    set month component

--- react-frontend/src/components/core/User.js
+++ react-frontend/src/components/core/User.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../navbar/Navbar'
+import SetMonth from '../other/SetMonth'
 
 const User = props => {
 
+  const [month, setMonth] = useState(new Date(Date.now()).toLocaleString('default', { month: 'long', year: 'numeric' }))
+
   return (<>
     <NavBar {...props} />
-    {props.match.params.user}
+
+    <SetMonth {...props} />
+
+
   </>)
 }
 
---
+++ react-frontend/src/components/other/SetMonth.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SetMonth = (props) => {
+
+    return (<>
+        Set month
+    </>)
+}
+
+export default SetMonth;
