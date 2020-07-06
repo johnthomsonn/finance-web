cd8d717dbaf84cb8d632509055f3b13adcff2cdc
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-06 10:32:53 +0100

    redirect to /username if already signe din

--- react-frontend/src/components/core/Signup.js
+++ react-frontend/src/components/core/Signup.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from '../navbar/Navbar'
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import "./Signup.css";
-import {isSignedIn, validateUsername, validateEmail} from "../../js/methods";
+import { isSignedIn, validateUsername, validateEmail } from "../../js/methods";
 
 const Signup = props => {
 
@@ -12,24 +12,24 @@ const Signup = props => {
     email: "",
     password: "",
     confirm: "",
-    redirectToProfile : false
+    redirectToProfile: false
   });
   const [error, setError] = useState("")
   const [showSubmit, setShowSubmit] = useState(false)
   const [validState, setValidState] = useState({
-    username : false,
-    email : false,
-    password : false
+    username: false,
+    email: false,
+    password: false
   })
 
   //effects
-  useEffect( () => shouldSubmitButtonAppear() , [validState])
+  useEffect(() => shouldSubmitButtonAppear(), [validState])
 
   //called whenever the user presses a key in the submit form
   const handleInput = name => evt => {
     const change = evt.target.value;
     //set the new input
-    setInput({...input, [name] : change});
+    setInput({ ...input, [name]: change });
 
     //sets the input state to be valid or not depending on user input
     setInputState(name, change)
@@ -38,62 +38,59 @@ const Signup = props => {
 
   //sets the input state by checking if the input is valid
   const setInputState = (name, change) => {
-    if( name === "username")
-      setValidState({...validState, username : isValidUsername(change)});
-    else if(name === "email")
-      setValidState({...validState, email : isValidEmail(change)});
-    else if(name ==="confirm")
-      setValidState({...validState, password : input.password.length>= 8 && input.password === change});
-    else if(name ==="password")
-      setValidState({...validState, password : change.length >= 8 && change === input.confirm});
+    if (name === "username")
+      setValidState({ ...validState, username: isValidUsername(change) });
+    else if (name === "email")
+      setValidState({ ...validState, email: isValidEmail(change) });
+    else if (name === "confirm")
+      setValidState({ ...validState, password: input.password.length >= 8 && input.password === change });
+    else if (name === "password")
+      setValidState({ ...validState, password: change.length >= 8 && change === input.confirm });
   }
 
   // checks to see if user input is valid by checking the valid state then sets the showSubmit state
-  const shouldSubmitButtonAppear =  () => {
-      setShowSubmit((validState.username && validState.password && validState.email) ? true : false)
+  const shouldSubmitButtonAppear = () => {
+    setShowSubmit((validState.username && validState.password && validState.email) ? true : false)
   }
 
   //id we get this far then all input is valid
   const submitSignup = evt => {
     evt.preventDefault();
 
-    const {username, password, email, confirm} = input;
+    const { username, password, email, confirm } = input;
 
     fetch(`${process.env.REACT_APP_SERVER_URL}/auth/signup`, {
-      method : "POST",
-      mode : "cors",
-      credentials : 'include',
-      headers : {
-        Accept : "application/json",
-        "Content-Type" : "application/json"
+      method: "POST",
+      mode: "cors",
+      credentials: 'include',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
       },
-      body : JSON.stringify({
+      body: JSON.stringify({
         username,
         email,
         password,
         confirm
       })
     })
-    .then(response => response.json())
-    .then(data => {
-      if(data.error)
-      {
-        setError(data.error);
-      }
-      else
-      {
-        if(typeof window !== undefined)
-        {
-          window.sessionStorage.setItem("user", JSON.stringify(data.user));
-          setInput({...input, redirectToProfile : true});
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          setError(data.error);
         }
-      }
-    })
-    .catch(err => console.log(err))
+        else {
+          if (typeof window !== undefined) {
+            window.sessionStorage.setItem("user", JSON.stringify(data.user));
+            setInput({ ...input, redirectToProfile: true });
+          }
+        }
+      })
+      .catch(err => console.log(err))
 
   };
 
-  const isValidUsername =  username => {
+  const isValidUsername = username => {
     let isValid = false;
     //will return an array of invalid chars if any match or null if nothing matches (We want null)
     const usernameValidation = validateUsername(username);
@@ -114,13 +111,11 @@ const Signup = props => {
     // returns true or false if whole regex matches
     const emailValidation = validateEmail(email);
     let newInputError = "";
-    if(emailValidation)
-    {
+    if (emailValidation) {
       newInputError = "";
       isValid = true;
     }
-    else
-    {
+    else {
       newInputError = "Invalid email";
     }
     setError(newInputError);
@@ -129,20 +124,19 @@ const Signup = props => {
 
 
 
-  if(input.redirectToProfile)
-  {
+  if (input.redirectToProfile) {
     return <Redirect to={`/${input.username}`} />
   }
 
 
-    if (isSignedIn()) {
-      return <Redirect to="/" />;
-    }
+  if (isSignedIn()) {
+    return <Redirect to={`/${input.username}`} />;
+  }
 
   return (
     <>
 
-    <NavBar {...props}/>
+      <NavBar {...props} />
       <div className="signupBackground">
         <div className="signup">
           <h1> Sign Up </h1>
@@ -212,7 +206,7 @@ const Signup = props => {
 
             <div
               className="alert alert-danger"
-              style={{display: error.length ? "" : "none"}}
+              style={{ display: error.length ? "" : "none" }}
             >
               {error}
             </div>
@@ -220,7 +214,7 @@ const Signup = props => {
             <button
               type="submit"
               className="btn btn-raised submit-button"
-              style={{display: showSubmit ? "" : "none"}}
+              style={{ display: showSubmit ? "" : "none" }}
             >
               {" "}
               Sign Up{" "}
