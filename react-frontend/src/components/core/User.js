6732f51de869866670c3f774b608b230b0dbe3c2
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-06 14:00:31 +0100

    pass down month

--- react-frontend/src/components/core/User.js
+++ react-frontend/src/components/core/User.js
@@ -6,10 +6,14 @@ import CreateTransaction from '../other/CreateTransaction'
 import AllTransactions from '../other/AllTransactions'
 import './User.css'
 import Footer from './Footer'
+import { transform } from 'lodash'
 
 const User = props => {
 
   const [month, setMonth] = useState(new Date(Date.now()).toLocaleString('default', { month: 'long', year: 'numeric' }))
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = transaction => setTransactions([...transactions, transaction]);
 
   return (<>
     <NavBar {...props} />
@@ -21,7 +25,7 @@ const User = props => {
       </div>
 
       <div class="create-transaction">
-        <CreateTransaction {...props} />
+        <CreateTransaction {...props} addTransaction={addTransaction} month={month} />
       </div>
 
       <div class="month-overall">
