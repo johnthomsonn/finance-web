60b964726503dac9cba1c94a7cb8686d3ecfbd9c
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-09 11:53:30 +0100

    server call to add transaction

--- react-frontend/src/components/other/CreateTransaction.js
+++ react-frontend/src/components/other/CreateTransaction.js
@@ -3,7 +3,7 @@ import './CreateTransaction.css'
 import { split } from 'lodash'
 import { transactionCategories } from "./Categories"
 
-//TODO: Make sure that the category matches the type ie you cannot set salary and expenditure
+//TODO: Make sure that the category matches the type ie you cannot add salary and expenditure
 
 const CreateTransaction = props => {
 
@@ -33,9 +33,41 @@ const CreateTransaction = props => {
     }
 
     const createTransaction = () => {
+        const splitDate = props.month.split(" ");
+        const type = document.getElementById("type").value;
+        const category = document.getElementById("category").value;
+        const day = document.getElementById("day").value;
+        const month = document.getElementById("month").value;
+        const date = splitDate[1] + "-" + getMonthNumber(month) + "-" + day;
         fetch(`${process.env.REACT_APP_SERVER_URL}/user/${JSON.parse(window.sessionStorage.getItem("user")).username}/transaction`, {
-
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                description: input.description,
+                amount: input.amount,
+                transactionType: type,
+                category: category,
+                date: date
+            })
         })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    setError(data.error);
+                }
+                else {
+                    props.addTransaction(data.transaction);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                setError(err);
+            })
     }
 
     const days = () => {
@@ -77,6 +109,8 @@ const CreateTransaction = props => {
         }
     }
 
+    const getMonthStrings = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
     return (<>
         <div className="alert alert-danger" style={{ display: error.length > 0 ? "" : "none", position: "absolute", top: "10%", borderRadius: "10px" }}>
             {error}
@@ -85,18 +119,28 @@ const CreateTransaction = props => {
             <h5>Add New Transaction</h5>
             <form onSubmit={submitCreateTransaction}>
                 <input type="number" value={input.amount} placeholder="Amount:" onChange={handleInputChange("amount")} />
+
                 <input type="text" value={input.description} placeholder="Description" autoComplete="off" style={{ width: "300px" }} onChange={handleInputChange("description")} />
-                <select name="day" style={{ width: "30px" }}>
+
+                <select name="day" style={{ width: "30px" }} id="day">
                     {days().map(day => <option key={day} value={day}>{day}</option>)}
                 </select>
-                <input type="text" value={props.month} disabled />
-                <select name="type" >
+
+                <select name="month" id="month" >
+                    {getMonthStrings.map((month, index) => <option id={index} value={month} >{month}</option>)}
+                </select>
+
+                <input type="text" value={props.month.split(" ")[1]} disabled style={{ width: "50px" }} />
+
+                <select name="type" id="type">
                     <option value="Income" >Income</option>
                     <option value="Expenditure" >Expenditure</option>
                 </select>
-                <select name="category" style={{ width: "70px" }}>
+
+                <select name="category" style={{ width: "70px" }} id="category">
                     {transactionCategories.map((t, i) => <option key={i} value={t}>{t}</option>)}
                 </select>
+
                 <button type="submit" > Add Transaction </button>
             </form>
         </div>
