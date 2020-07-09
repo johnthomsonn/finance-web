0d03b055a74ae190ff2dae60155705eac604d635
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-09 11:52:49 +0100

    alter transaction type enum

--- models/transactionmodelschema.js
+++ models/transactionmodelschema.js
@@ -1,35 +1,35 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema
-const {transactionCategories} = require('./transactioncategories')
+const { ObjectId } = mongoose.Schema
+const { transactionCategories } = require('./transactioncategories')
 
 
 transactionSchema = new mongoose.Schema({
-  description : {
-    type : String,
-    required : true
-  },
-  amount : {
-    type : Number,
+  description: {
+    type: String,
     required: true
   },
-  user : {
-    type : ObjectId,
-    ref : "User"
+  amount: {
+    type: Number,
+    required: true
+  },
+  user: {
+    type: ObjectId,
+    ref: "User"
   },
   transactionType: {
-    type : String,
-    enum : ['income' , 'expenditure'],
-    required : true
+    type: String,
+    enum: ['income', 'expenditure', 'Income', 'Expenditure'],
+    required: true
   },
-  category : {
-    type : String,
-    enum : transactionCategories,
-    required : true
+  category: {
+    type: String,
+    enum: transactionCategories,
+    required: true
   },
-  created : {
+  created: {
     type: Date,
-    default : Date.now
+    default: Date.now
   }
 })
 
