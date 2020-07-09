c826321e06fa993f692b8918e51fb4e975ca35e2
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-09 20:32:56 +0100

    route to transaction categories

--- routes/transactionroute.js
+++ routes/transactionroute.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router();
 
-const {addTransaction,getTransactionById,deleteTransaction, getAllTransactionsForMonth } = require('../controllers/transactioncontroller')
-const {getTransactionCreationErrors,transactionValidation} = require('../validation/transactionvalidation')
+const { addTransaction, getTransactionById, deleteTransaction, getAllTransactionsForMonth } = require('../controllers/transactioncontroller')
+const { getTransactionCreationErrors, transactionValidation, getTransactionCategories } = require('../validation/transactionvalidation')
 
-router.post("/", getTransactionCreationErrors, transactionValidation,  addTransaction)
-router.delete("/:transactionId",  deleteTransaction)
+router.post("/", getTransactionCreationErrors, transactionValidation, addTransaction)
+router.delete("/:transactionId", deleteTransaction)
 router.get("/", getAllTransactionsForMonth)
+router.get("/types", getTransactionCategories)
 
-router.param("transactionId", getTransactionById )
+router.param("transactionId", getTransactionById)
 
 module.exports = router;
