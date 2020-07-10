481d3064aa94697037c9748c0e2c42f3b13f7722
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-10 19:01:02 +0100

    display all transactions

--- react-frontend/src/components/other/Transaction.js
+++ react-frontend/src/components/other/Transaction.js
@@ -1,10 +1,19 @@
-import React from "React"
+import React from "react"
 import "./Transaction.css"
 
 const Transaction = props => {
 
-    return (<>
+    const { id, amount, category, description, date, type } = props;
 
+    return (<>
+        <tr>
+            <th scope="row">{id}</th>
+            <td>{amount}</td>
+            <td>{date}</td>
+            <td>{description}</td>
+            <td>{category}</td>
+            <td>{type}</td>
+        </tr>
     </>)
 }
 export default Transaction;
