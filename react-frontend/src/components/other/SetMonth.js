07f3df5addcc067619fbe3eb1864ff86905a8214
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-10 15:23:04 +0100

    pre select current month

--- react-frontend/src/components/other/SetMonth.js
+++ react-frontend/src/components/other/SetMonth.js
@@ -18,16 +18,19 @@ const SetMonth = (props) => {
         setMonths(monthArray);
     }
 
+
     const handleMonthChange = () => {
         const selected = document.getElementById("setMonth").value;
         props.updateMonth(selected)
     }
 
+
+
     return (<>
         <div className="set-month-div">
             <h5>Choose Month</h5>
 
-            <select name="month" id="setMonth" onChange={handleMonthChange}>
+            <select name="month" id="setMonth" onChange={handleMonthChange} value={props.month}>
                 {months.map((m, i) => <option key={i} value={m} >{m}</option>)}
             </select>
 
