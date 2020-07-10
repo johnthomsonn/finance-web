76d59074916000373fab74a8b5cac98b8bfab639
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-10 14:59:17 +0100

    months display

--- react-frontend/src/components/other/SetMonth.js
+++ react-frontend/src/components/other/SetMonth.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "./SetMonth.css";
 
 const SetMonth = (props) => {
 
+    const [months, setMonths] = useState([]);
+
+    useEffect(() => setMonthState(), []);
+
+    const setMonthState = () => {
+        const monthArray = [];
+        const currentYear = new Date(Date.now()).getFullYear();
+        for (let y = currentYear - 1; y <= currentYear; y++) {
+            for (let m = 0; m < 12; m++) {
+                monthArray.push(new Date(y, m, 1).toLocaleString('default', { month: 'long', year: 'numeric' }));
+            }
+        }
+        setMonths(monthArray);
+    }
+
     return (<>
-        Set month
+        <div className="set-month-div">
+            <h5>Choose Month</h5>
+
+            <select name="month" id="setMonth">
+                {months.map((m, i) => <option key={i} value={m}>{m}</option>)}
+            </select>
+
+        </div>
     </>)
 }
 
