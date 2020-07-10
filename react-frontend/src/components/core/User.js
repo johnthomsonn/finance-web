7ad43547e43d41b3e86737a205a8cbb5d457393c
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-10 19:22:32 +0100

    fetch for all transactions for given month

--- react-frontend/src/components/core/User.js
+++ react-frontend/src/components/core/User.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../navbar/Navbar'
 import SetMonth from '../other/SetMonth'
 import MonthOverall from '../other/MonthOverall'
@@ -11,6 +11,9 @@ const User = props => {
 
   const [month, setMonth] = useState(new Date(Date.now()).toLocaleString('default', { month: 'long', year: 'numeric' }))
   const [transactions, setTransactions] = useState([]);
+  const [error, setError] = useState("")
+
+  useEffect(() => getAllTransactions(), []);
 
   const addTransaction = transaction => setTransactions([...transactions, transaction]);
 
@@ -18,11 +21,41 @@ const User = props => {
     setMonth(newMonth);
   }
 
+  const getAllTransactions = () => {
+    const monthSplit = month.split(" ");
+    const m = monthSplit[0].substr(0, 3) + "-" + monthSplit[1].substr(2, 4)
+    fetch(`${process.env.REACT_APP_SERVER_URL}/user/${JSON.parse(window.sessionStorage.getItem("user")).username}/month/${m}/transactions`, {
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          setError(data.error)
+        }
+        else {
+          setTransactions(data.transactions)
+          setError("")
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
   return (<>
     <NavBar {...props} />
     <div class="container-fluid user-container">
 
       <div class="set-month" >
+        <div
+          className="alert alert-danger"
+          style={{ display: error.length ? "" : "none" }}
+        >
+          {error}
+        </div>
         <SetMonth {...props} updateMonth={updateSelectedMonth} month={month} />
       </div>
 
