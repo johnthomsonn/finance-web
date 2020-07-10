392a802f5ecd2bab18c8f029c2d5fba9600d5469
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-10 18:45:07 +0100

    transaction component and stylesheet

--- react-frontend/src/components/other/AllTransactions.js
+++ react-frontend/src/components/other/AllTransactions.js
@@ -7,6 +7,27 @@ const AllTransactions = props => {
         <div className="all-transactions-div">
             <h5>All Transactions for {props.month}</h5>
 
+            <div className="table-div">
+                <table className="table">
+
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Amount</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Type</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                    </tbody>
+
+                </table>
+            </div>
+
         </div>
     </>);
 }
---
+++ react-frontend/src/components/other/Transaction.js
@@ -0,0 +1,10 @@
+import React from "React"
+import "./Transaction.css"
+
+const Transaction = props => {
+
+    return (<>
+
+    </>)
+}
+export default Transaction;
