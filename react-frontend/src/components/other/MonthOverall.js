08b4b1da86c79af756c3df388e52a001129c4ee4
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-13 11:30:51 +0100

    start of table

--- react-frontend/src/components/other/MonthOverall.js
+++ react-frontend/src/components/other/MonthOverall.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./MonthOverall.css"
 
 const MonthOverall = props => {
 
+
+    const [transactions, setTransactions] = useState([])
+    const [sorted, setSorted] = useState({})
+
+    useEffect(() => setTransactions(props.transactions), [])
+    useEffect(() => sortTransactions(), [])
+
+    const sortTransactions = () => {
+        let obj = {};
+        props.transactions.map(t => {
+            const cat = t.category;
+            const amount = t.amount;
+            if (obj.hasOwnProperty(cat)) {
+                let total = obj[cat];
+                total += amount;
+                obj = {
+                    ...obj, [cat]: total
+                };
+            }
+            else {
+                obj = { ...obj, [cat]: amount }
+            }
+        })
+        //setSorted(obj)
+        return obj;
+    }
+
+    const doTable = () => {
+        const categories = sortTransactions();
+
+
+    }
+
     return (<>
         <div className="month-overall-div">
+            <h5>{props.month} Summary</h5>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Category</th>
+                        <th scope="col">Total</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                </tbody>
+            </table>
 
         </div>
     </>)
