8a1717993e60e5dd2c2c2cffe2d197dcd46be434
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-15 17:17:52 +0100

    remove balance sessionStorage on signout

--- react-frontend/src/js/methods.js
+++ react-frontend/src/js/methods.js
@@ -4,17 +4,17 @@ exports.isSignedIn = () => {
   let isLogged = undefined;
   allCookies.forEach(cookieStr => {
     const cookies = cookieStr.split('=');
-    if(cookies[0] == "localToken")
+    if (cookies[0] == "localToken")
       isLogged = cookies[1];
   })
 
-  if(isLogged !== undefined)
+  if (isLogged !== undefined)
     return isLogged
   return false
 }
 
 exports.validateUsername = username => {
-return username.match(/[@<>'";,:\\]/gi)
+  return username.match(/[@<>'";,:\\]/gi)
 }
 
 exports.validateEmail = email => {
@@ -22,14 +22,14 @@ exports.validateEmail = email => {
 }
 
 exports.signOut = (next) => {
-  if(typeof window !== undefined)
-  {
+  if (typeof window !== undefined) {
     window.sessionStorage.removeItem("user")
+    window.sessionStorage.removeItem("balance")
     next();
     return fetch(`${process.env.REACT_APP_SERVER_URL}/auth/signout`, {
-      credentials : 'include',
-      mode : 'cors'
+      credentials: 'include',
+      mode: 'cors'
     })
-    .catch(err => console.log(err))
+      .catch(err => console.log(err))
   }
 }
