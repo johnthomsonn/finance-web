d312fd1fc39e1944caba40d2217e8f8371acd03e
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-15 22:32:47 +0100

    show balance on render

--- react-frontend/src/components/core/EditProfile.js
+++ react-frontend/src/components/core/EditProfile.js
@@ -1,9 +1,19 @@
-import React from "React";
-import "./EditProfile.css"
+import React, { useState, useEffect } from "react";
+import "./EditProfile.css";
+import NavBar from "../navbar/Navbar";
 
 const EditProfile = props => {
 
+    const [balance, setBalance] = useState("");
+
+    useEffect(() => setBalanceOnRender(), []);
+
+    const setBalanceOnRender = () => {
+        setBalance(window.sessionStorage.getItem("balance"));
+    }
+
     return (<>
+        <NavBar {...props} balance={balance} />
         <div className="edit-profile-container">
 
             Edit Profile
