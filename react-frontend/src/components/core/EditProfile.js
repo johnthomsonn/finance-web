9cab5a42f32f8a54de2b72646ec6e8c60acfa295
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-15 22:23:29 +0100

    initial EditProfile component

---
+++ react-frontend/src/components/core/EditProfile.js
@@ -0,0 +1,13 @@
+import React from "React";
+import "./EditProfile.css"
+
+const EditProfile = props => {
+
+    return (<>
+        <div className="edit-profile-container">
+
+            Edit Profile
+        </div>
+    </>);
+};
+export default EditProfile;
