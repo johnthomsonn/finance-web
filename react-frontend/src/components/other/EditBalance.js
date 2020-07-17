53158db1091fda4600e623ad4e1f2427d6848b84
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-17 16:39:24 +0100

    initial component comit

---
+++ react-frontend/src/components/other/EditBalance.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EditBalance = props => {
+
+    return (<>
+
+        <div className="edit-balance-div">
+            Edit balance:
+        </div>
+
+    </>);
+};
+
+export default EditBalance;
