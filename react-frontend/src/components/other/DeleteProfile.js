2657fe1976809e9dfa4599a9e5fa65ad45c1cc27
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-17 19:46:10 +0100

    delete profile initial commit

---
+++ react-frontend/src/components/other/DeleteProfile.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./DeleteProfile.css"
+
+const DeleteProfile = props => {
+
+
+    return (<>
+
+        <div className="delete-profile-div">
+
+            Delete Profile
+        </div>
+
+    </>);
+};
+
+export default DeleteProfile;
