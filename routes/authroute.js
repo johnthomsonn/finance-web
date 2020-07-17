160650b528c4bf3dd7a186fb2f6fb1119b4341b4
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-17 18:22:24 +0100

    update balance route

--- routes/authroute.js
+++ routes/authroute.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const { signup, signin, signout, deleteUser } = require("../controllers/authcontroller");
 const { getSignupErrors, signupValidation, getSignInErrors, signinValidation } = require("../validation/authvalidation");
+const { updateBalance } = require("../controllers/usercontroller");
 
 
 router.post("/signup", getSignupErrors, signupValidation, signup);
 router.post("/signin", getSignInErrors, signinValidation, signin);
+router.patch("/", updateBalance);
 router.get("/signout", signout);
 router.delete("/", deleteUser);
 
