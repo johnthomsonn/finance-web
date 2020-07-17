ae8105c2aae769dc1e1f17d3c25b79dbcff988f7
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-17 19:42:27 +0100

    update balance and send updated balance up tree

--- react-frontend/src/components/other/EditBalance.js
+++ react-frontend/src/components/other/EditBalance.js
@@ -5,7 +5,7 @@ const EditBalance = props => {
 
 
     const [error, setError] = useState("");
-
+    const [message, setMessage] = useState("");
 
 
     const updateBalance = event => {
@@ -17,7 +17,31 @@ const EditBalance = props => {
     }
 
     const serverCallToUpdateBalance = bal => {
-
+        fetch(`${process.env.REACT_APP_SERVER_URL}/user/${JSON.parse(window.sessionStorage.getItem("user")).username}/edit/balance`, {
+            method: "PATCH",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ balance: bal })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    setError(data.error);
+                }
+                else {
+                    updateBalanceOnSuccess(data.balance);
+                    document.getElementById("balance").value = "";
+                    setMessage("Balance update succesful, you now have £" + data.balance);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                setError("There was an issue with the server request.");
+            });
     }
 
     const validateBalance = bal => {
@@ -32,6 +56,10 @@ const EditBalance = props => {
         return isValid;
     }
 
+    const updateBalanceOnSuccess = updatedBalance => {
+        props.updateBalance(updatedBalance);
+    }
+
     return (<>
 
         <div className="edit-balance-div">
@@ -40,7 +68,7 @@ const EditBalance = props => {
                     Edit balance:
                 </label>
 
-                <input type="number" name="balance-input" id="balance" value={props.balance} className="balance-input" step=".01" />
+                <input type="number" name="balance-input" id="balance" placeholder={props.balance} className="balance-input" step=".01" />
                 <button type="submit" >Update</button>
 
             </form>
@@ -49,6 +77,15 @@ const EditBalance = props => {
                 style={{ display: error.length ? "" : "none" }}
             >
                 {error}
+            </div>
+            <div
+                className="alert alert-primary"
+                style={{ display: message.length ? "" : "none" }}
+            >
+                {message}
+                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
         </div>
 
