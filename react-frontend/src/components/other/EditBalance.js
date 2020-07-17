7683f709f2108d7df0aa04581205f8a6f60a31dd
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-07-17 17:38:05 +0100

    allow 2 d.p on balance update

--- react-frontend/src/components/other/EditBalance.js
+++ react-frontend/src/components/other/EditBalance.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import "./EditBalance.css";
 
 const EditBalance = props => {
+
+
+    const [error, setError] = useState("");
+
+    const updateBalance = event => {
+        event.preventDefault();
+        const bal = document.getElementById("balance").value;
+        if (validateBalance(bal)) {
+            serverCallToUpdateBalance(bal);
+        }
+    }
+
+    const serverCallToUpdateBalance = bal => {
+
+    }
+
+    const validateBalance = bal => {
+        let isValid = false;
+        if (!(/^\d+(\.\d{1,2})?$/gi).test(bal)) {
+            setError("Invalid balance");
+        }
+        else {
+            setError("")
+            isValid = true;
+        }
+        return isValid;
+    }
 
     return (<>
 
         <div className="edit-balance-div">
-            Edit balance:
+            <form onSubmit={updateBalance} >
+                <label htmlFor="balance">
+                    Edit balance:
+                </label>
+
+                <input type="number" name="balance-input" id="balance" className="balance-input" step=".01" />
+                <button type="submit" >Update</button>
+
+            </form>
+            <div
+                className="alert alert-danger"
+                style={{ display: error.length ? "" : "none" }}
+            >
+                {error}
+            </div>
         </div>
 
     </>);
