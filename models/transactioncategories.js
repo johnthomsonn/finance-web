42a0af074867ee712514d15c591f6c516da9c5b1
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-08-27 16:03:45 +0100

    added categories

--- models/transactioncategories.js
+++ models/transactioncategories.js
@@ -1,2 +1,2 @@
-exports.incomeCategories = ['salary', 'other', 'interest', 'savings', 'from shanice'];
-exports.expenditureCategories = ['bills', 'food', 'other', 'bad', 'ok', 'utilities', 'to shanice', 'finance'];
+exports.incomeCategories = ['salary', 'other', 'interest', 'savings', 'from shanice', 'from savings'];
+exports.expenditureCategories = ['bills', 'food', 'other', 'bad', 'ok', 'utilities', 'to shanice', 'finance', 'to savings'];
