12d421ea9c917847415580384796a95dab248e99
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-09-29 09:52:59 +0100

    added save the pennies expenditure

--- models/transactioncategories.js
+++ models/transactioncategories.js
@@ -1,2 +1,2 @@
 exports.incomeCategories = ['salary', 'other', 'interest', 'savings', 'from shanice', 'from savings'];
-exports.expenditureCategories = ['bills', 'food', 'other', 'bad', 'ok', 'utilities', 'to shanice', 'credit', 'to savings', 'petrol', 'take-away', 'occasions'];
+exports.expenditureCategories = ['bills', 'food', 'other', 'bad', 'ok', 'utilities', 'to shanice', 'credit', 'to savings', 'petrol', 'take-away', 'occasions', 'save-the-pennies'];
