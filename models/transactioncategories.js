c0b8528e3b2c1b5f42eabf3bec8e08f5601cc241
johnthomsonn <johnthomson16@hotmail.co.uk>
2021-10-17 12:14:51 +0100

    removed save-the-pennies

--- models/transactioncategories.js
+++ models/transactioncategories.js
@@ -1,2 +1,2 @@
 exports.incomeCategories = ['salary', 'other in', 'interest', 'from shanice', 'from savings'];
-exports.expenditureCategories = ['bills', 'save the pennies','food shop', 'other out', 'bad', 'treat', 'utilities', 'to shanice', 'credit', 'to savings', 'petrol', 'take-away', 'occasions', 'save-the-pennies'];
+exports.expenditureCategories = ['bills', 'save the pennies','food shop', 'other out', 'bad', 'treat', 'utilities', 'to shanice', 'credit', 'to savings', 'petrol', 'take-away', 'occasions'];
